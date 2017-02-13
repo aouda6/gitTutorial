1d87075686fbabe36d8e2cebd77aba6c7e545f1f
Abdulla Ouda <aouda6@uwo.ca>
2017-02-12 21:45:19 -0500

    Added Course class and made changes to Student and Instructor

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Abdulla on 2017-02-12.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+        this.cname = cname;
+        this.description = description;
+    }
+}
